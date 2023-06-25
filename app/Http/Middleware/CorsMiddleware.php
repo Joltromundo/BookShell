c621c2eea900a97e37004a714190ee398517a4cd
Joltromundo <jonathan.tibia@hotmail.com>
2023-06-25 20:27:38 -0300

    Verify same host in sample route

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -8,15 +8,19 @@ class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
-        $allowedOrigin = env('APP_URL');
+        $allowedOrigins = [
+            '127.0.0.1:8000',
+        ];
 
-        if ($request->header('Origin') !== $allowedOrigin) {
+        $origin = $request->header('Host');
+
+       if (!in_array($origin, $allowedOrigins)) {
             return response()->json(['error' => 'Access denied'], 403);
         }
 
         $response = $next($request);
 
-        $response->header('Access-Control-Allow-Origin', $allowedOrigin);
+        $response->header('Access-Control-Allow-Origin', $allowedOrigins);
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
         $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
